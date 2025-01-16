903061297c324059676e4869b077623dc48b6a21
BanBetrayed <davyliu.school@gmail.com>
2025-01-16 16:46:27 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+package main;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("here now is the numbers 1 to 10");
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(String.valueOf(i));
+		}
+		System.out.println("amazing, now give me an A+");
+	}
+}
